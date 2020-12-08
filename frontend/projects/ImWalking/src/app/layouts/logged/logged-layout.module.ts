7261a01922c624674368944a4fa0fc97002de4c9
Oleh Sarnovskyi <oleh.sarnovskyi@etiya.com>
2020-12-08 17:29:21 +0200

    Added toolbar

--- frontend/projects/ImWalking/src/app/layouts/logged/logged-layout.module.ts
+++ frontend/projects/ImWalking/src/app/layouts/logged/logged-layout.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 
 import { LoggedLayoutRoutingModule } from './logged-layout-routing.module';
 import {LoggedLayoutComponent} from "./logged-layout.component";
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [LoggedLayoutComponent],
-  imports: [LoggedLayoutRoutingModule]
+  imports: [
+    LoggedLayoutRoutingModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule
+  ]
 })
 export class LoggedLayoutModule { }
