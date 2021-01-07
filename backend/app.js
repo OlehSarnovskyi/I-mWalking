85961f44849bbe79893100da377e97c832cc3c6d
Oleh Sarnovskyi <oleg.sarnovskiy@gmail.com>
2021-01-07 13:18:57 +0200

    Finished auth on the BE side

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const passport = require('passport')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-const app = express()
 const authRoutes = require('./routes/auth')
+const keys = require('./config/keys')
+
+const app = express()
+
+mongoose.connect(keys.mongoURI)
+    .then(() => console.log('MongoDB connected!!!'))
+    .catch(error => console.log(`MongoDB failed!!! \n${error}`))
+
+app.use(passport.initialize())
+require('./middleware/passport')(passport)
 
 app.use(morgan('dev'))
 app.use(cors())
--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,14 +1,59 @@
-module.exports.register = (req, res) => {
-    res.status(200).json({
-        register: {
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+const keys = require('../config/keys')
+const errorHandler = require('../utils/errorHandler')
+
+module.exports.register = async (req, res) => {
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        res.status(409).json({
+            message: 'This email already exist, try another please.'
+        })
+    } else {
+        const salt = bcryptjs.genSaltSync(10)
+        const password = req.body.password
+
+        const user = new User({
             email: req.body.email,
-            password: req.body.password
+            password: bcryptjs.hashSync(password, salt),
+            name: req.body.name
+        })
+        try {
+            await user.save()
+            res.status(201).json({
+                message: 'You created a new account'
+            })
+        } catch (e) {
+            errorHandler(res, e)
         }
-    })
+    }
 }
 
-module.exports.login = (req, res) => {
-    res.status(200).json({
-        login: true
-    })
+module.exports.login = async (req, res) => {
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        const passwordResult = bcryptjs.compareSync(req.body.password, candidate.password)
+        if (passwordResult) {
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 60 * 60})
+
+            res.status(200).json({
+                accessToken: `Bearer ${token}`,
+                message: 'You signed in'
+            })
+        } else {
+            res.status(401).json({
+                message: 'Passwords don\'t match'
+            })
+        }
+    } else {
+        res.status(404).json({
+            message: 'User with that email not found'
+        })
+    }
 }
