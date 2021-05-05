4eadc3d6a81eb5b7a1a198c9a49757072f133721
Oleh Sarnovskyi <oleh.sarnovskyi@etiya.com>
2021-05-05 04:09:01 +0300

    Implemented pending for search button.

--- frontend/projects/ImWalking/src/app/layouts/logged/pages/posts/components/posts-search-form/posts-search-form.component.ts
+++ frontend/projects/ImWalking/src/app/layouts/logged/pages/posts/components/posts-search-form/posts-search-form.component.ts
@@ -1,21 +1,33 @@
-import {Component, OnInit, ChangeDetectionStrategy, Output, EventEmitter} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Actions, ofActionDispatched, ofActionSuccessful} from "@ngxs/store";
+import {SearchPostsAction} from "../../store";
+import {BehaviorSubject, Subscription} from "rxjs";
+import {debounceTime} from "rxjs/operators";
 
 @Component({
   selector: 'app-posts-search-form',
   templateUrl: './posts-search-form.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PostsSearchFormComponent implements OnInit {
+export class PostsSearchFormComponent implements OnInit, OnDestroy {
 
   @Output() submitted = new EventEmitter<void>()
 
+  pendingSearch$ = new BehaviorSubject(false)
+  subscription = new Subscription()
+
   form: FormGroup
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private actions$: Actions) {}
 
   ngOnInit(): void {
     this.initForm()
+    this.listenSearchAction()
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
   }
 
   initForm(): void {
@@ -23,5 +35,21 @@ export class PostsSearchFormComponent implements OnInit {
       city: [null, Validators.required],
       description: ['']
     })
+  }
+
+  listenSearchAction() {
+    const sub1 = this.actions$.pipe(
+      ofActionDispatched(SearchPostsAction)
+    ).subscribe(() => {
+      this.pendingSearch$.next(true)
+    })
+    const sub2 = this.actions$.pipe(
+        ofActionSuccessful(SearchPostsAction)
+    ).subscribe(() => {
+      this.pendingSearch$.next(false)
+    })
+
+    this.subscription.add(sub1)
+    this.subscription.add(sub2)
   }
 }
