092a3e51adfbd14a68b4df486e84987ab78e0ad2
Oleh Sarnovskyi <oleh.sarnovskyi@etiya.com>
2021-05-15 02:41:26 +0300

    Changed styles for auth layout.

--- frontend/projects/ImWalking/src/app/layouts/auth/auth-layout.component.ts
+++ frontend/projects/ImWalking/src/app/layouts/auth/auth-layout.component.ts
@@ -1,12 +1,29 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {Store} from "@ngxs/store";
-import {SetTokenAction} from "./pages/login/store";
+import {SetTokenAction} from "./pages";
 import {JwtHelperService} from "@auth0/angular-jwt";
 
 @Component({
   selector: 'app-auth-layout',
   templateUrl: './auth-layout.component.html',
+  styles: [`
+    .container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
+    mat-card {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      height: 80vh;
+      width: 500px;
+      min-width: 300px;
+      max-width: 500px;
+    }
+  `],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AuthLayoutComponent implements OnInit {
--- frontend/projects/ImWalking/src/app/layouts/auth/auth-layout.module.ts
+++ frontend/projects/ImWalking/src/app/layouts/auth/auth-layout.module.ts
@@ -2,10 +2,16 @@ import {NgModule} from '@angular/core';
 
 import {AuthLayoutRoutingModule} from './auth-layout-routing.module';
 import {AuthLayoutComponent} from "./auth-layout.component";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
 
 
 @NgModule({
   declarations: [AuthLayoutComponent],
-  imports: [AuthLayoutRoutingModule]
+  imports: [
+    AuthLayoutRoutingModule,
+    MatCardModule,
+    MatButtonModule
+  ]
 })
 export class AuthLayoutModule { }
