aac06bbc977904c6e3e61fc91447d175c9b89096
Oleh Sarnovskyi <oleh.sarnovskyi@etiya.com>
2021-06-06 00:08:27 +0300

    make it better

--- backend/app.js
+++ backend/app.js
@@ -30,9 +30,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('../frontend/dist/ImWalking'))
 
     app.get('*', (req, res) => {
-        res.sendFile(
-            path.join(__dirname+ '../frontend/dist/ImWalking/index.html')
-        )
+        res.sendFile('../frontend/dist/ImWalking/index.html')
     })
 }
 
