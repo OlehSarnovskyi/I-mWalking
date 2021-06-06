c271030568d0f4409abc0e50ef2bfddd33297045
Oleh Sarnovskyi <oleh.sarnovskyi@etiya.com>
2021-06-06 23:32:27 +0300

    make it better

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,7 @@ const app = express()
 mongoose.connect(keys.mongoURI, {
     useNewUrlParser: true,
     useCreateIndex: true,
+    useUnifiedTopology: true,
     useFindAndModify: false
 })
     .then(() => console.log('MongoDB connected!!!'))
