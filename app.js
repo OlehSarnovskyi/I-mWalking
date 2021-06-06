9002a015bc75546b5f54c55fb3aebddfa557e3e7
Oleh Sarnovskyi <oleh.sarnovskyi@etiya.com>
2021-06-06 11:19:29 +0300

    make it better

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ app.use('/api/auth', authRoutes)
 app.use('/api/posts', postsRoutes)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(`${__dirname}/../frontend/dist/ImWalking`))
+    app.use(express.static(`frontend/dist/ImWalking`))
 
     app.get('*', (req, res) => {
         res.sendFile(
-            path.join(__dirname, 'frontend', 'dist', 'ImWalking', 'index.html')
+            path.join('frontend', 'dist', 'ImWalking', 'index.html')
         )
     })
 }
