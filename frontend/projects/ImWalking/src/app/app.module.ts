962fb1f7451306e560df0d397ba4cbede9786e3b
Oleh Sarnovskyi <oleh.sarnovskyi@etiya.com>
2021-06-09 12:22:03 +0300

    Refactor snack-bar-errors-interceptor version.

--- frontend/projects/ImWalking/src/app/app.module.ts
+++ frontend/projects/ImWalking/src/app/app.module.ts
@@ -12,6 +12,8 @@ import {environment} from "../environments/environment";
 import {LoggedInGuard, NotLoggedInGuard} from "./guards";
 import {LoginService, LoginState} from "./layouts";
 import {JwtModule} from "@auth0/angular-jwt";
+// @ts-ignore
+import {SnackBarErrorsInterceptorModule} from "snack-bar-errors-interceptor";
 
 const STATES = [
   LoginState
@@ -51,7 +53,7 @@ const GUARDS = [
       }
     }),
     ...NGXS_MODULES,
-    // SnackBarErrorsInterceptorModule
+    SnackBarErrorsInterceptorModule
   ],
   providers: [
     ...SERVICES,
