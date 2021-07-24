8154f0577f6f24db985018e3c80cc48c0cf6d813
Oleh Sarnovskyi <oleh.sarnovskyi@etiya.com>
2021-07-25 02:49:44 +0300

    Created default-image.directive

---
+++ frontend/projects/ImWalking/src/app/layouts/logged/pages/settings/directives/default-image.directive.ts
@@ -0,0 +1,23 @@
+import {Directive, ElementRef, HostListener, OnInit} from '@angular/core';
+
+@Directive({
+  selector: '[appDefaultImage]'
+})
+export class DefaultImageDirective implements OnInit {
+
+  defaultImage = 'https://img.icons8.com/pastel-glyph/2x/person-male--v2.png'
+
+  @HostListener('error') handleError(e) {
+    if (e === undefined) {
+      this.elementRef.nativeElement.src = this.defaultImage
+    }
+  }
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(): void {
+    if (!this.elementRef.nativeElement.src.trim()) {
+      this.elementRef.nativeElement.src = this.defaultImage
+    }
+  }
+}
