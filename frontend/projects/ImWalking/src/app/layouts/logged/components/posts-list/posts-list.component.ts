3b2f6ff94f3fd64249485ca532716a03aaaebeb5
Oleh Sarnovskyi <oleh.sarnovskyi@etiya.com>
2021-07-27 14:40:11 +0300

    Updated app-posts-list actions

--- frontend/projects/ImWalking/src/app/layouts/logged/components/posts-list/posts-list.component.ts
+++ frontend/projects/ImWalking/src/app/layouts/logged/components/posts-list/posts-list.component.ts
@@ -1,11 +1,14 @@
 import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
 import {Posts} from "../../pages";
+import {LoginState} from "../../../auth";
+import {JwtHelperService} from "@auth0/angular-jwt";
+import {Store} from "@ngxs/store";
 
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
-  styles: [
-    `.example-card {
+  styles: [`
+    .example-card {
       max-width: 400px;
     }
 
@@ -13,21 +16,27 @@ import {Posts} from "../../pages";
       background-image: url('https://material.angular.io/assets/img/examples/shiba1.jpg');
       background-size: cover;
     }
+
     mat-list-item {
       height: auto !important;
       display: flex !important;
       justify-content: center;
       margin-top: 10px;
     }
-    `
-  ],
+  `],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PostsListComponent {
 
-  @Input() actions: Posts.Action[]
-
   @Input() posts: Posts.PostsList;
 
   @Output() actionEmitter = new EventEmitter<Posts.Action>()
+
+  constructor(private store$: Store,
+              private jwtHelper: JwtHelperService) {}
+
+  actions(authorPostId: string): Posts.Action[] {
+    const myId = this.jwtHelper.decodeToken(this.store$.selectSnapshot(LoginState.token)).userId
+    return myId === authorPostId ? ['edit', 'delete'] : ['like']
+  }
 }
